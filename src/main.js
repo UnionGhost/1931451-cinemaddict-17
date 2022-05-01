a550b9a35fcc18416c5e4274867b7fc2d2ee43dc
UnionGhost <takeofme@gmail.com>
2022-05-01 17:23:01 +0300

    Сделал отрисовку страницы

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import HeaderProfileView from './view/header-profile-view.js';
 import MainNavigationView from './view/main-navigation-view.js';
 import FilmsPresenter from './presenter/films-presenter.js';
 import FooterStatisticsView from './view/footer-statistics-view.js';
-import FilmDetailsPresenter from './presenter/film-details-presenter.js';
+//import FilmDetailsPresenter from './presenter/film-details-presenter.js';
 import {render} from './render.js';
 
 
@@ -10,7 +10,7 @@ const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
 const siteFooterElement = document.querySelector('.footer');
 const filmsPresenter = new FilmsPresenter();
-const filmDetailsPresenter = new FilmDetailsPresenter();
+//const filmDetailsPresenter = new FilmDetailsPresenter();
 
 
 render(new HeaderProfileView(), siteHeaderElement);
